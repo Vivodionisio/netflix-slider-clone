888712e7ed7b339bcefaefe7c6c65155e881e663
Vivodionisio <saulnewbury@gmail.com>
2022-05-23 11:36:13 +0100

    new comments

--- src/components/content-item/ContentItem.jsx
+++ src/components/content-item/ContentItem.jsx
@@ -14,10 +14,10 @@ function ContentItem({ content, style, isDisabled, imageConfig }) {
 
   const inMeRef = useRef() // stores (remembers) if mouse is inside a content item
   const thumbRef = useRef() // ref for original ContentItem initial coordinates
-  const containerRef = useRef() // ref for getting expanded coordinates
+  const containerRef = useRef() // ref for getting mid-expanded coordinates
+  let midRef = useRef() // for storing mid-expanded coordinates
   const modalRef = useRef() // ref for modal itself
   const dOfModal = gsap.utils.selector(modalRef) // to select decendent elements
-  let midRef = useRef()
 
   // Presentation data
   const { backdrop_path, release_dates, genres, runtime } = itemContent
@@ -49,12 +49,6 @@ function ContentItem({ content, style, isDisabled, imageConfig }) {
   useEffect(() => {
     if (!modalRef.current || !isOpen) return
 
-    gsap.defaults({ ease: 'none' })
-    gsap.to(dOfModal('.modal-gb'), {
-      backgroundColor: 'rgb(0 0 0 / 63%)',
-      delay: 0.1
-    })
-
     gsap.set(dOfModal('.inner-wrapper'), {
       position: 'fixed'
     })
@@ -66,14 +60,21 @@ function ContentItem({ content, style, isDisabled, imageConfig }) {
       left: '50%',
       xPercent: -50,
       scaleX: 2.6,
-      scaleY: 2.6
+      scaleY: 2.6,
+      duration: 0.3
     })
 
-    gsap.to(dOfModal('.gradient'), { opacity: 1, duration: 0.4, delay: 0.1 })
+    gsap.to(dOfModal('.gradient'), { opacity: 1, duration: 0.2, delay: 0.1 })
     gsap.to(dOfModal('.closeBtn, engagements'), {
       opacity: 1,
-      duration: 0.5,
-      delay: 0.1
+      delay: 0.1,
+      duration: 0.3
+    })
+
+    gsap.set(dOfModal('.modal-bg'), { backgroundColor: 'rgb(0 0 0 / 63%)' })
+
+    gsap.to(dOfModal('.modal-bg'), {
+      autoAlpha: 1
     })
   }, [isOpen])
 
@@ -106,16 +107,12 @@ function ContentItem({ content, style, isDisabled, imageConfig }) {
   }
 
   function handleOpenModal() {
+    // This function sets initial dimensions and position for modal based on boundingClientRect of the content item at in its hovered state. Setting is open then triggers useEffect, which deals with the transition to expanded modal.
     midRef = containerRef.current.getBoundingClientRect()
-
-    const position = midRef.left + midRef.width / 2 // middle of box relative to window
-    const percentage = window.innerWidth / position
-    const mapPercentageToBox = percentage * midRef.width
 
     gsap.set(modalRef.current, { display: 'flex' })
 
-    gsap.set(dOfModal('.modal-bg'), { display: 'block' })
-    gsap.to(dOfModal('.modal-bg'), { backgroundColor: 'rgb(0 0 0 / 0%)' })
+    gsap.set(dOfModal('.modal-bg'), { autoAlpha: 0, display: 'block' })
 
     gsap.set(dOfModal('.inner-wrapper'), {
       position: 'fixed',
@@ -149,9 +146,9 @@ function ContentItem({ content, style, isDisabled, imageConfig }) {
       width: `${midRef.width}px`,
       top: `${rect.top}px`,
       left: `${rect.left}px`,
-      scaleX: 0.6366666667, // quirk - expected .66666667 to work
-      scaleY: 0.6366666667,
-      xPercent: -18, // quirk - expected 0 to work
+      scaleX: 0.6266666667, // fiddly - expected .66666667 to work
+      scaleY: 0.6266666667,
+      xPercent: -18.2, // fiddly - expected 0 to work
       duration: 0.5,
       ease: 'none'
     })
